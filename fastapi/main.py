a4dbad5e0f651e48e30b5377f4bf7e49cd8aa0c5
José Flores <josefloressv@gmail.com>
2024-11-15 23:25:58 -0600

    FastAPI: add format to the response

--- fastapi/main.py
+++ fastapi/main.py
@@ -1,11 +1,25 @@
 from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
-@app.get("/")
+@app.get("/", response_class=JSONResponse)
 def get_list():
     return [1, 2, 3]
 
-@app.get("/contact")
+@app.get("/contact", response_class=HTMLResponse)
 def contact():
-    return "Jose Flores"
+    return """
+    <html>
+        <head>
+            <title>Contact</title>
+        </head>
+        <body>
+            <h1>Contact</h1>
+            <p>Send us an email at
+                <a href="mailto:hola@test.com">hola@test.com</a>
+            </p>
+        </body>
+    </html>
+    """
