990d4f52cc94949e98a5271b61343b8b6e7db706
José Flores <josefloressv@gmail.com>
2024-11-23 13:19:26 -0600

    Add assertion examples and unit test samples

---
+++ unit_tests/assert_base.py
@@ -0,0 +1,10 @@
+"""
+Assert statement is used to check the condition. If the condition is true,
+the program will continue execution. If the condition is false,
+the program will stop and throw an AssertionError.
+"""
+
+if __name__ == "__main__":
+    assert  10 == 10, "Should be equal" #AssertionError: Should be equal
+
+    print("Everything passed")
